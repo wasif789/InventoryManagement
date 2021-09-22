976f7ed2090ca072f584ad4144c681da81b4e8d1
wasif farid <wsffarid1@gmail.com>
2021-09-22 17:22:44 +0530

    Inventory Management

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InventoryManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
