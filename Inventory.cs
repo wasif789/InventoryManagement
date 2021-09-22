223fab1dcd2b1711dc1c6c6d8b5ddff2cdbff2ae
wasif farid <wsffarid1@gmail.com>
2021-09-22 18:10:16 +0530

    inventory manager

--- Inventory.cs
+++ Inventory.cs
@@ -17,31 +17,125 @@ namespace InventoryManagement
 
             //deserializing the json onject and reading the test from file
             InventoryUtility inventoryUtility = JsonConvert.DeserializeObject<InventoryUtility>(File.ReadAllText(filePath));
-            Console.WriteLine("Display\n1. Rice\n2. Pulse\n3.Wheat");
-
-            //getting the option from the user
+            Console.WriteLine("\n1.Display \n2. Add Inventory\n3. Update Inventory\n4. Delete an Inventory");
+            Console.WriteLine("Enter your chioce:");
+            //getting the option from the user for operation
             int choice = Convert.ToInt32(Console.ReadLine());
 
-            //calls the method for the particular option
+            Console.WriteLine("Enter the Inventory:");
+            //getting input from user for which inventory
+            Console.WriteLine("\n1.RICE \n2.PULSES\n3.WHEAT");
+            int inventory = Convert.ToInt32(Console.ReadLine());
+
+
             switch (choice)
             {
                 case 1:
-                    manager.DisplayInventory(inventoryUtility.riceList);
+                    //calls the method for the particular option
+                    switch (inventory)
+                    {
+                        case 1:
+                            Console.WriteLine("======DISPLAYING RICE INVENTORY=======");
+                            manager.DisplayInventory(inventoryUtility.riceList);
+                            break;
+
+                        case 2:
+                            Console.WriteLine("======DISPLAYING PULSE INVENTORY=======");
+                            manager.DisplayInventory(inventoryUtility.pulseList);
+                            break;
+
+                        case 3:
+                            Console.WriteLine("======DISPLAYING WHEAT INVENTORY=======");
+                            manager.DisplayInventory(inventoryUtility.wheatList);
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid Option");
+                            break;
+
+                    }
                     break;
 
+                //selection for adding the item to inventory
                 case 2:
-                    manager.DisplayInventory(inventoryUtility.pulseList);
+                    switch (inventory)
+                    {
+                        case 1:
+                            Console.WriteLine("=========ADDING to RICE INVENTORY========");
+                            inventoryUtility.riceList = manager.AddToInventory(inventoryUtility.riceList);
+                            File.WriteAllText(filePath, JsonConvert.SerializeObject(inventoryUtility));
+                            break;
+                        case 2:
+                            Console.WriteLine("=========ADDING to PULSE INVENTORY========");
+                            inventoryUtility.pulseList = manager.AddToInventory(inventoryUtility.pulseList);
+                            File.WriteAllText(filePath, JsonConvert.SerializeObject(inventoryUtility));
+                            break;
+                        case 3:
+                            Console.WriteLine("=========ADDING to WHEAT INVENTORY========");
+                            inventoryUtility.wheatList = manager.AddToInventory(inventoryUtility.wheatList);
+                            File.WriteAllText(filePath, JsonConvert.SerializeObject(inventoryUtility));
+                            break;
+                        default:
+                            Console.WriteLine("Invalid option");
+                            break;
+                    }
+                    Console.WriteLine("Inventory is updated");
+                    break;
+                //selection for updating the inventory
+                case 3:
+                    switch (inventory)
+                    {
+                        case 1:
+                            inventoryUtility.riceList = manager.UpdateInventory(inventoryUtility.riceList);
+                            inventoryUtility.riceList = manager.AddToInventory(inventoryUtility.riceList);
+                            File.WriteAllText(filePath, JsonConvert.SerializeObject(inventoryUtility));
+                            break;
+                        case 2:
+                            inventoryUtility.pulseList = manager.UpdateInventory(inventoryUtility.pulseList);
+                            inventoryUtility.pulseList = manager.AddToInventory(inventoryUtility.pulseList);
+                            File.WriteAllText(filePath, JsonConvert.SerializeObject(inventoryUtility));
+                            break;
+                        case 3:
+                            inventoryUtility.wheatList = manager.UpdateInventory(inventoryUtility.wheatList);
+                            inventoryUtility.wheatList = manager.AddToInventory(inventoryUtility.wheatList);
+                            File.WriteAllText(filePath, JsonConvert.SerializeObject(inventoryUtility));
+                            break;
+                        default:
+                            Console.WriteLine("Invalid option");
+                            break;
+
+                    }
+                    Console.WriteLine("Inventory Updated");
                     break;
 
-                case 3:
-                    manager.DisplayInventory(inventoryUtility.wheatList);
+                //selection for deleting the inventory
+                case 4:
+                    switch (inventory)
+                    {
+                        case 1:
+                            manager.RemoveInventory(inventoryUtility.riceList);
+                            File.WriteAllText(filePath, JsonConvert.SerializeObject(inventoryUtility));
+                            break;
+                        case 2:
+                            manager.RemoveInventory(inventoryUtility.pulseList);
+                            File.WriteAllText(filePath, JsonConvert.SerializeObject(inventoryUtility));
+                            break;
+                        case 3:
+                            manager.RemoveInventory(inventoryUtility.wheatList);
+                            File.WriteAllText(filePath, JsonConvert.SerializeObject(inventoryUtility));
+                            break;
+                        default:
+                            Console.WriteLine("Invalid option");
+                            break;
+                    }
+                    Console.WriteLine("Inventory Deleted");
                     break;
 
                 default:
                     Console.WriteLine("Invalid Option");
                     break;
-
             }
+
         }
     }
 }
--- InventoryManager.cs
+++ InventoryManager.cs
@@ -6,6 +6,113 @@ namespace InventoryManagement
 {
     class InventoryManager
     {
+        //method to add item to rice inventory
+        public List<InventoryUtility.Rice> AddToInventory(List<InventoryUtility.Rice> riceList)
+        {
+            //create the new object for rice class 
+            InventoryUtility.Rice rice = new InventoryUtility.Rice();
+            Console.WriteLine("Enter the name of rice:");
+            rice.Name = Console.ReadLine();
+            Console.WriteLine("Enter the weight of rice in kg:");
+            rice.Weight = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the price of the rice:");
+            rice.Price = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the type of rice:");
+            rice.type = Console.ReadLine();
+            //append the updated object to the list
+            riceList.Add(rice);
+            return riceList;
+        }
+
+        //method to add item to pulse inventory
+        public List<InventoryUtility.Pulse> AddToInventory(List<InventoryUtility.Pulse> pulseList)
+        {
+            //create the new object for pulse class 
+            InventoryUtility.Pulse pulse = new InventoryUtility.Pulse();
+            Console.WriteLine("Enter the name of pulse:");
+            pulse.Name = Console.ReadLine();
+            Console.WriteLine("Enter the weight of pulse in kg:");
+            pulse.Weight = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the price of the pulse:");
+            pulse.Price = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the type of pulse:");
+            pulse.type = Console.ReadLine();
+            //append the updated object to the list
+            pulseList.Add(pulse);
+            return pulseList;
+        }
+
+        //method to add item to wheat inventory
+        public List<InventoryUtility.Wheat> AddToInventory(List<InventoryUtility.Wheat> wheatList)
+        {
+            //create the new object for wheat class 
+            InventoryUtility.Wheat wheat = new InventoryUtility.Wheat();
+            Console.WriteLine("Enter the name of wheat:");
+            wheat.Name = Console.ReadLine();
+            Console.WriteLine("Enter the weight of wheat in kg:");
+            wheat.Weight = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the wheat of the rice:");
+            wheat.Price = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the wheat of rice:");
+            wheat.type = Console.ReadLine();
+            //append the updated object to the list
+            wheatList.Add(wheat);
+            return wheatList;
+        }
+
+        //method to update the rice inventory
+        public List<InventoryUtility.Rice> UpdateInventory(List<InventoryUtility.Rice> riceList)
+        {
+            Console.WriteLine("Enter the name of rice to be updated:");
+            string Name = Console.ReadLine();
+            riceList.Remove(riceList.Find(utl => utl.Name.Equals(Name)));
+            return (riceList);
+        }
+
+        //method to update the Pulse inventory
+        public List<InventoryUtility.Pulse> UpdateInventory(List<InventoryUtility.Pulse> pulseList)
+        {
+            Console.WriteLine("Enter the name of pulse to be updated:");
+            string Name = Console.ReadLine();
+            pulseList.Remove(pulseList.Find(utl => utl.Name.Equals(Name)));
+            return (pulseList);
+        }
+
+        //method to update the Wheat inventory
+        public List<InventoryUtility.Wheat> UpdateInventory(List<InventoryUtility.Wheat> wheatList)
+        {
+            Console.WriteLine("Enter the name of wheat to be updated:");
+            string Name = Console.ReadLine();
+            wheatList.Remove(wheatList.Find(utl => utl.Name.Equals(Name)));
+            return (wheatList);
+        }
+
+        //method to delete the rice inventory
+        public List<InventoryUtility.Rice> RemoveInventory(List<InventoryUtility.Rice> riceList)
+        {
+            Console.WriteLine("Enter the name of rice to be Removed:");
+            string Name = Console.ReadLine();
+            riceList.Remove(riceList.Find(utl => utl.Name.Equals(Name)));
+            return (riceList);
+        }
+
+        //method to delete the Pulse inventory
+        public List<InventoryUtility.Pulse> RemoveInventory(List<InventoryUtility.Pulse> pulseList)
+        {
+            Console.WriteLine("Enter the name of pulse to be Removed:");
+            string Name = Console.ReadLine();
+            pulseList.Remove(pulseList.Find(utl => utl.Name.Equals(Name)));
+            return (pulseList);
+        }
+
+        //method to delete the Wheat inventory
+        public List<InventoryUtility.Wheat> RemoveInventory(List<InventoryUtility.Wheat> wheatList)
+        {
+            Console.WriteLine("Enter the name of wheat to be Removed:");
+            string Name = Console.ReadLine();
+            wheatList.Remove(wheatList.Find(utl => utl.Name.Equals(Name)));
+            return (wheatList);
+        }
         //method to display the rice list
         public void DisplayInventory(List<InventoryUtility.Rice> rice)
         {
